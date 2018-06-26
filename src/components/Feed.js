dcb5d44ddae214e2a8975b96fb1e0c236a8a5e37
Jason Hansen <jason_hansen@outlook.com>
2018-06-25 18:03:35 -0700

    [fetching] step 4, display loading experience

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'lore-hook-connect';
+import PayloadStates from '../constants/PayloadStates';
 import Tweet from './Tweet';
 
 class Feed extends React.Component {
@@ -13,6 +14,17 @@ class Feed extends React.Component {
 
   render() {
     const { tweets } = this.props;
+
+    if (tweets.state === PayloadStates.FETCHING) {
+      return (
+        <div className="feed">
+          <h2 className="title">
+            Feed
+          </h2>
+          <div className="loader"/>
+        </div>
+      );
+    }
 
     return (
       <div className="feed">
